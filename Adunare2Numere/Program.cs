1245bae63d428b9aa428c0a86de87f9a1d56434d
briansomogyi <brian.somogyi@emanuel.ro>
2023-10-12 10:34:24 +0300

    Done some changes

---
+++ Adunare2Numere/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Adunare2Numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a=");a=int.Parse(Console.ReadLine()); 
+            Console.Write("b=");b=int.Parse(Console.ReadLine());
+            if (a <= int.MaxValue - b) 
+            {
+                Console.WriteLine(a + b);
+            }
+            else
+            {
+                Console.WriteLine("ArithmeticOverflow");
+            }
+        }
+    }
+}
